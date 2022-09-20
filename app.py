f76e484ed1f8d37e621634c459c2b086df2b5d05
Imanol-Ortega <imanicoortemohr018@gmail.com>
2022-09-20 17:42:44 -0400

    Comienzo

---
+++ app.py
@@ -0,0 +1,15 @@
+from distutils.log import debug
+from flask import Flask, render_template,request
+from flask_socketio import SocketIO
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'asdf34325'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    socketio.run(app,debug=True,port=5000)
