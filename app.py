e07437f70749172b4312bf819210e7952063de68
Adrian <adriancasco788@gmail.com>
2022-09-20 19:36:44 -0400

    Agrege un comentario linea 1

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+# - Adrian
+
+
 from distutils.log import debug
 from flask import Flask, render_template,request
 from flask_socketio import SocketIO
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'asdf34325'
