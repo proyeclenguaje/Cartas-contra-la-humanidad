9cd111f15063b33df07e19ef920a1f3db2246f81
Natalia <nataliaval1912@gmail.com>
2022-10-17 19:36:01 -0300

    creacion de cronometro

--- static/player.js
+++ static/player.js
@@ -3,7 +3,12 @@
 // const player3=
 // const player4=
 const juez=1+Math.floor(Math.random()*4);
+contador =0;
 
+window.setInterval(function(){
+     contador=contador+1; 
+     crono.innerHTML=contador 
+},1000)
 function envio()
 {
     var conte= document.getElementById("C1");
