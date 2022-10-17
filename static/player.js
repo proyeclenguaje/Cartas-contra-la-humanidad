6c8f1afe80527cc7e168acaff8307ae5aabf6535
Adrian <adriancasco788@gmail.com>
2022-10-17 18:44:24 -0300

    b

---
+++ static/player.js
@@ -0,0 +1,13 @@
+// const player1=
+// const player2=
+// const player3=
+// const player4=
+const juez=1+Math.floor(Math.random()*4);
+
+function envio()
+{
+    var conte= document.getElementById("C1");
+    var con=conte.innerHTML
+    var cart=document.getElementById("player1");
+    cart.innerHTML=con;
+}
